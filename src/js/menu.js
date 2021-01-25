b6a1d6b01925284754b6a20cbb8d936d5f3642d6
G.Sterczewski <sterczewski.dev@protonmail.ch>
2021-01-25 10:56:41 +0100

    [dev] hamburger button styling

--- src/js/menu.js
+++ src/js/menu.js
@@ -4,6 +4,7 @@ window.addEventListener("DOMContentLoaded",()=>{
   menuButton.addEventListener("click",()=>{
   
     navigation.classList.toggle("mobile-navigation--active");
+    menuButton.classList.toggle("hamburger-menu--open");
     document.querySelector("body").classList.toggle("no-vertical-overflow");
   })
 })
